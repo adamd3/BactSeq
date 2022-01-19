07d941b950b4bfe67dfd69129042a09c8db2549d
Adam Dinan <adam1989ie@gmail.com>
2022-01-19 08:11:46 +0000

    remove sample data

--- test_nf_pipeline.sh
+++
@@ -1,7 +0,0 @@
-/home/adam/nextflow run /home/adam/strain_seq \
-    --data_dir /projects/pseudomonas_transcriptomics/storage/fastq_files \
-    --meta_file /projects/pseudomonas_transcriptomics/storage/hzi_meta.txt \
-    --gpa_file /projects/pseudomonas_transcriptomics/storage/gene_presence_absence.csv \
-    --st_file /home/adam/pseudomonas_transcriptomics/CF_STs.txt \
-    --perc 99 \
-    -profile docker

8473832385baef83a5cfad8ce346b0f6f9536035
Adam Dinan <adam1989ie@gmail.com>
2023-10-04 15:51:56 +0100

    Fix test sample sheet

--- bin/make_meta_file.py
+++ bin/make_meta_file.py
@@ -23,12 +23,6 @@ def make_meta(sample_file, data_dir, outf):
     # remove empty rows
     sample_dat.replace("", float("NaN"), inplace=True)
     sample_dat.dropna(subset = ["sample"], inplace=True)
-    # remove empty columns 
-    sample_dat.dropna(axis=1, how='all', inplace=True)
-    # ensure that paths to files are correct
-    f1_bnames = [os.path.basename(f) for f in sample_dat['file1'].to_list()]
-    f1_full_paths = [os.path.join(data_dir, b) for b in f1_bnames]
-    sample_dat['file1'] = f1_full_paths
     if(len(sample_dat.file2.value_counts()) > 0):
         f2_bnames = [os.path.basename(f) for f in sample_dat['file2'].to_list()]
         f2_full_paths = [os.path.join(data_dir, b) for b in f2_bnames]
@@ -36,6 +30,12 @@ def make_meta(sample_file, data_dir, outf):
         sample_dat['paired'] = "1"
     else:
         sample_dat['paired'] = "0"
+    # remove empty columns 
+    sample_dat.dropna(axis=1, how='all', inplace=True)
+    # ensure that paths to files are correct
+    f1_bnames = [os.path.basename(f) for f in sample_dat['file1'].to_list()]
+    f1_full_paths = [os.path.join(data_dir, b) for b in f1_bnames]
+    sample_dat['file1'] = f1_full_paths
     sample_dat.to_csv(outf, index=False, sep='\t')
 
 if __name__ == "__main__":

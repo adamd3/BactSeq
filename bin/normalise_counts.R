3df833f294fbe9927062a9d11b45a0df8a5eb4d9
Adam Dinan <adam1989ie@gmail.com>
2023-10-06 12:06:31 +0100

    Add DESeq2 normalisation of read counts per gene

--- bin/normalise_counts.R
+++ bin/normalise_counts.R
@@ -39,25 +39,42 @@ counts_tab[["feature_id"]] <- NULL
 counts_tab <- as.data.frame(sapply(counts_tab, as.numeric))
 rownames(counts_tab) <- gene_names
 
-# ## remove rRNA genes
+## remove rRNA genes
 ref_tab_sub <- ref_gene_tab[!ref_gene_tab$biotype == "rRNA", ]
 non_rRNA_counts <- counts_tab[rownames(counts_tab) %in% ref_tab_sub$locus_tag, ]
 
-# ## passing the protein-coding counts matrix only
-# non_rRNA_counts <- counts_tab
-# ref_tab_sub <- ref_gene_tab[ref_gene_tab$biotype=="protein_coding",]
-
-
 ## ensure that ref gene annotations order matches counts table
-ref_tab_sub <- ref_tab_sub[match(rownames(non_rRNA_counts), ref_tab_sub$locus_tag), ]
+ref_tab_sub <- ref_tab_sub[match(
+    rownames(non_rRNA_counts), ref_tab_sub$locus_tag
+), ]
+
+## DESeq2 scaled counts
+colData <- data.frame(sample_name = colnames(non_rRNA_counts))
+
+dds <- DESeqDataSetFromMatrix(
+    countData = round(non_rRNA_counts),
+    colData = colData, design = ~1
+)
+
+dds <- estimateSizeFactors(dds)
+deseq_norm <- counts(dds, normalized = TRUE)
+deseq_df <- as_tibble(as.data.frame(log2(deseq_norm + 1)))
+deseq_df$feature_id <- rownames(deseq_df)
+deseq_df <- deseq_df[c("feature_id", meta_tab1$sample_id)]
+
+write.table(
+    deseq_df, file.path(outdir, "deseq_counts.tsv"),
+    col.names = TRUE,
+    row.names = FALSE, sep = "\t", quote = FALSE
+)
 
 ## counts per million (cpm) - normalised for lib size but not gene length
 y <- DGEList(counts = non_rRNA_counts)
 y <- calcNormFactors(y, method = "TMM")
 cpm_df <- as.data.frame(edgeR::cpm(y, log = log))
-
-
 cpm_df <- tibble::rownames_to_column(as.data.frame(cpm_df), "feature_id")
+
+
 write.table(
     cpm_df, file.path(outdir, "cpm_counts.tsv"),
     col.names = TRUE,
@@ -65,7 +82,6 @@ write.table(
 )
 
 ## reads per kilobase per million (rpkm) - normalised for lib size + gene length
-
 y <- DGEList(
     counts = non_rRNA_counts,
     genes = data.frame(gene.length = as.numeric(ref_tab_sub$gene_length))

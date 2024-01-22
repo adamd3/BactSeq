5f1e447d1820e906e2f54857de7d8ce73499715a
Adam Dinan <adam1989ie@gmail.com>
2024-01-22 13:58:14 -0600

    Fix normalisation subsetting

--- bin/normalise_counts.R
+++ bin/normalise_counts.R
@@ -44,6 +44,7 @@ rownames(counts_tab) <- gene_names
 # ref_tab_sub <- ref_gene_tab[!ref_gene_tab$biotype == "rRNA", ]
 # non_rRNA_counts <- counts_tab[rownames(counts_tab) %in% ref_tab_sub$locus_tag, ]
 ref_tab_sub <- ref_gene_tab
+non_rRNA_counts <- counts_tab
 
 ## ensure that ref gene annotations order matches counts table
 ref_tab_sub <- ref_tab_sub[match(

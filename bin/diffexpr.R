8200c106b58425e02208fac6e4ebe325ec34e20f
adamd3 <adam1989ie@gmail.com>
2026-07-24 14:35:39 +0100

    fix: create contrast list cleanly

--- bin/diffexpr.R
+++ bin/diffexpr.R
@@ -44,8 +44,6 @@ l2fc_thresh <- opt$log2fc_threshold
 outdir <- opt$outdir
 
 
-
-
 ## -----------------------------------------------------------------------------
 ## Read and process data
 ## -----------------------------------------------------------------------------
@@ -84,27 +82,25 @@ meta_tab <- meta_tab %>%
 stopifnot(identical(colnames(counts_tab), rownames(meta_tab)))
 
 
-
 ## -----------------------------------------------------------------------------
 ## Differential gene expression
 ## -----------------------------------------------------------------------------
-comb_list <- lapply(1:nrow(contrast_tab), function(idx) {
-    c(contrast_tab[idx, 1], contrast_tab[idx, 2])
-})
-comb_names <- lapply(1:nrow(contrast_tab), function(idx) {
-    contrast_name <- paste0(contrast_tab[idx, 1], "_", contrast_tab[idx, 2])
-    contrast_name
-})
-names(comb_list) <- comb_names
+comb_list <- contrast_tab %>%
+    mutate(contrast_name = paste(.[[1]], .[[2]], sep = "_")) %>%
+    {
+        setNames(
+            map2(.[[1]], .[[2]], c),
+            .$contrast_name
+        )
+    }
 
 dds <- DESeqDataSetFromMatrix(
     countData = round(counts_tab),
-    colData = meta_tab,
-    design = ~group
+    colData   = meta_tab,
+    design    = ~group
 )
 dds <- DESeq(dds)
 
-## perform pairwise contrasts of groups
 contrast_list <- lapply(comb_list, function(x) {
     gp_1 <- x[1]
     gp_2 <- x[2]
@@ -112,97 +108,47 @@ contrast_list <- lapply(comb_list, function(x) {
     res
 })
 
-## export tables of genes with log2FC and p-values:
 lapply(seq_along(contrast_list), function(x) {
     contrast_name <- names(contrast_list)[x]
+
     res_df <- tibble::rownames_to_column(
-        as.data.frame(contrast_list[x]), "feature_id"
+        as.data.frame(contrast_list[[x]]), "feature_id"
     )
-    colnames(res_df) <- gsub(contrast_name, "", colnames(res_df))
-    colnames(res_df) <- gsub("\\.", "", colnames(res_df))
+
     write.table(
         res_df,
-        file = file.path(outdir, paste0("DGE_", contrast_name, ".tsv")),
-        quote = FALSE, sep = "\t",
-        row.names = FALSE, col.names = TRUE
+        file      = file.path(outdir, paste0("DGE_", contrast_name, ".tsv")),
+        quote     = FALSE,
+        sep       = "\t",
+        row.names = FALSE,
+        col.names = TRUE
     )
 })
-
 
 
 ## -----------------------------------------------------------------------------
 ## Volcano plots
 ## -----------------------------------------------------------------------------
-lfc_list <- lapply(contrast_list, function(x) {
-    subset(x, padj < p_thresh)$log2FoldChange
-})
-xmin <- round_any(min(unlist(lfc_list)), 0.1, floor)
-xmax <- round_any(max(unlist(lfc_list)), 0.1, ceiling)
-
-if (abs(xmax) > abs(xmin)) {
-    xmin <- xmax * -1
-} else if (abs(xmin) > abs(xmax)) {
-    xmax <- xmin * -1
-}
-
-ymin <- 0
-pval_list <- lapply(contrast_list, function(x) {
-    subset(x, padj < p_thresh)$padj
-})
-ymax <- round_any(max(unlist(pval_list)), 0.5, ceiling)
-
 set1pal <- brewer.pal(9, "Set1")
 
-
 lapply(seq_along(contrast_list), function(x) {
-    ymin <- 0
-    ymax <- max(-log10(
-        subset(
-            contrast_list[[x]],
-            padj < p_thresh & log2FoldChange > l2fc_thresh
-        )$padj
-    ))
-    xmin <- round_any(
-        min(unlist(subset(
-            contrast_list[[x]], padj < p_thresh & log2FoldChange > l2fc_thresh
-        )$log2FoldChange)),
-        0.1, floor
-    )
-    xmax <- round_any(
-        max(unlist(subset(
-            contrast_list[[x]], padj < p_thresh & log2FoldChange > l2fc_thresh
-        )$log2FoldChange)),
-        0.1, ceiling
-    )
-    if (abs(xmax) > abs(xmin)) {
-        xmin <- xmax * -1
-    } else if (abs(xmin) > abs(xmax)) {
-        xmax <- xmin * -1
-    }
+    res_obj <- contrast_list[[x]]
 
-    ## show gene labels where the log2FoldChange exceeds a certain threshold:
+    # Get max -log10 padj safely (avoiding -Inf errors)
+    valid_p <- na.omit(res_obj$padj)
+    ymax <- ifelse(length(valid_p) > 0, max(-log10(valid_p[valid_p > 0])), 10)
+
+    # Custom gene label selection
     keepLabs <- rownames(
-        subset(
-            contrast_list[[x]],
-            padj < p_thresh & (abs(log2FoldChange) > xmax * 0.9)
-        )
+        subset(res_obj, padj < p_thresh & abs(log2FoldChange) > l2fc_thresh)
     )
-    keepLabs <- c(keepLabs, rownames(
-        subset(
-            contrast_list[[x]],
-            padj < p_thresh & (abs(-log10(padj)) > ymax * 0.5)
-        )
-    ))
 
-    ## Custom colour scheme
+    # Custom colour scheme
     keyvals <- ifelse(
-        contrast_list[[x]]$log2FoldChange < (
-            l2fc_thresh * -1) & contrast_list[[x]]$padj < p_thresh,
+        res_obj$log2FoldChange < (-1 * l2fc_thresh) & res_obj$padj < p_thresh,
         set1pal[3],
         ifelse(
-            contrast_list[[x]]$log2FoldChange > l2fc_thresh & contrast_list[[
-                x
-            ]]$padj < p_thresh,
+            res_obj$log2FoldChange > l2fc_thresh & res_obj$padj < p_thresh,
             set1pal[1], "grey45"
         )
     )
@@ -215,39 +161,41 @@ lapply(seq_along(contrast_list), function(x) {
     group2 <- (comb_list[[x]])[2]
 
     volcano_plot <- EnhancedVolcano::EnhancedVolcano(
-        contrast_list[[x]],
-        lab = rownames(contrast_list[[x]]),
-        selectLab = keepLabs,
-        x = "log2FoldChange", y = "padj",
-        pointSize = 3.0,
-        labSize = 4.0,
-        pCutoff = p_thresh,
-        FCcutoff = l2fc_thresh,
-        colCustom = keyvals,
-        drawConnectors = TRUE,
-        max.overlaps = 20,
-        arrowheads = FALSE,
+        res_obj,
+        lab                = rownames(res_obj),
+        selectLab          = keepLabs,
+        x                  = "log2FoldChange",
+        y                  = "padj",
+        pointSize          = 3.0,
+        labSize            = 4.0,
+        pCutoff            = p_thresh,
+        FCcutoff           = l2fc_thresh,
+        colCustom          = keyvals,
+        drawConnectors     = TRUE,
+        max.overlaps       = 20,
+        arrowheads         = FALSE,
         min.segment.length = 1.5,
-        title = paste0(group1, " vs ", group2),
-        subtitle = ""
+        title              = paste0(group1, " vs ", group2),
+        subtitle           = ""
     ) +
         theme(
-            axis.text.x = element_text(size = 12 * 1.5),
-            axis.title.x = element_text(size = 12 * 1.5),
-            axis.text.y = element_text(size = 12 * 1.5),
-            axis.title.y = element_text(size = 12 * 1.5),
+            axis.text.x = element_text(size = 18),
+            axis.title.x = element_text(size = 18),
+            axis.text.y = element_text(size = 18),
+            axis.title.y = element_text(size = 18),
             plot.subtitle = element_blank(),
             plot.caption = element_blank(),
             legend.position = "none",
-            plot.title = element_text(size = 12 * 1.5, hjust = 0.5)
+            plot.title = element_text(size = 18, hjust = 0.5)
         ) + ylab(bquote(~ -Log[10] ~ adjusted ~ italic(P)))
 
     ggsave(
         volcano_plot,
-        file = file.path(outdir, paste0("volcano_plot_", names(
-            contrast_list
-        )[x], ".png")),
-        device = "png", units = "in",
-        width = 8, height = 7, dpi = 300
+        file   = file.path(outdir, paste0("volcano_plot_", names(contrast_list)[x], ".png")),
+        device = "png",
+        units  = "in",
+        width  = 8,
+        height = 7,
+        dpi    = 300
     )
 })
